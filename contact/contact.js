510de06caa3d73cb530e2481042062c58fafdb4e
Brandon Choi <jc440@duke.edu>
2015-07-18 21:39:58 -0700

    added email response when clicked

--- contact/contact.js
+++ contact/contact.js
@@ -7,3 +7,9 @@ function goBack() {
  $("#back").click(function() {
  	goBack();
  });
+
+ $(document).ready(function(){
+    $("#email").on('click',function(){
+       window.location.href = "mailto:brandon.choi@duke.edu?subject=&body=Hi%20Brandon,"; 
+    });
+});
