56a6b89272b960a5a509cda98dfc64fdfc8cc85c
Brandon Choi <jc440@duke.edu>
2016-05-29 23:06:32 -0400

    fixed _blank link on writings

--- js/scroller.js
+++ js/scroller.js
@@ -1,5 +1,11 @@
 $("body").fadeIn(1100);
 
+// $(function () {
+//     setInterval(function () {
+//         $("#instruction-text").fadeIn(1000).delay(2000).fadeOut(1500).delay(2000).fadeIn(1500);
+//     }, 5000);
+// });
+
 $(document).ready(function(){
 	 $("#email").on('click',function(){
 			window.location.href = "mailto:brandon.choi@duke.edu?subject=&body=Hi%20Brandon,";
